665b7df96058304131e4bbc9b0fc48ddbabd31f9
thaisNY <71556315+thaisNY@users.noreply.github.com>
2021-10-19 23:13:50 -0300

    hashset
    implementação de uma hashset

---
+++ hash/hashset.py
@@ -0,0 +1,35 @@
+class MyHashSet:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.size = 10000
+        self.table = [None] * self.size
+    
+    def calculate_hash_value(self, key):
+        return key % self.size
+
+    def add(self, key: int) -> None:
+        hv = self.calculate_hash_value(key)
+        
+        if self.table[hv] == None:
+            self.table[hv] = [key]
+        else:
+            self.table[hv].append(key)
+
+    def remove(self, key: int) -> None:
+        hv = self.calculate_hash_value(key)
+        
+        if self.table[hv] != None:
+            while key in self.table[hv]: 
+                self.table[hv].remove(key)
+
+    def contains(self, key: int) -> bool:
+        """
+        Returns true if this set contains the specified element
+        """
+        hv = self.calculate_hash_value(key)
+        
+        if self.table[hv] != None:
+            return key in self.table[hv]
+        return False
